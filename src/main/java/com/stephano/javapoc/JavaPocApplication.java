1799efae1a10aaf3f12e0f478e5efec055dfe152
stephanogiuseppe <stephano.grp@gmail.com>
2020-02-24 16:38:32 -0300

    initial commit

---
+++ src/main/java/com/stephano/javapoc/JavaPocApplication.java
@@ -0,0 +1,13 @@
+package com.stephano.javapoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaPocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaPocApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/stephano/javapoc/JavaPocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stephano.javapoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaPocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
